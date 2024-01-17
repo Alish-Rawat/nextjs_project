169fe773f28fa8c698c37cf22c2d2b68325d605b
Alish-Rawat <rawatalish4@gmail.com>
2024-01-17 22:03:17 +0530

    cache fix5

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -26,9 +26,12 @@ const Feed = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch("/api/prompt");
+      const response = await fetch("/api/prompt", {
+        cache: "no-store",
+      });
       const data = await response.json();
       setPosts(data);
+      console.log(data);
     };
 
     fetchPosts();
