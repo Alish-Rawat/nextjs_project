f9e5d3c4fae37efd05f7972daa1079bb1ced3efb
Alish-Rawat <rawatalish4@gmail.com>
2024-01-18 01:49:21 +0530

    profile

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -63,13 +63,13 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       {session?.user.id === post.creator._id && pathName === "/profile" && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p
-            className="font-inter text-sm green_gradient cursor-pointer"
+            className="font-satoshi text-md font-semibold text-blue-500  cursor-pointer"
             onClick={handleEdit}
           >
             Edit
           </p>
           <p
-            className="font-inter text-sm orange_gradient cursor-pointer"
+            className="text-md font-satoshi font-semibold text-red-500  cursor-pointer"
             onClick={handleDelete}
           >
             Delete
