ee3d21c9a23cde14ce2a8f85d755401a32ff3686
Alish-Rawat <rawatalish4@gmail.com>
2024-09-01 04:31:56 +0530

    mongodb got paused, now fixed

--- utils/database.js
+++ utils/database.js
@@ -1,24 +1,25 @@
-import  mongoose  from "mongoose";
+import mongoose from "mongoose";
 
-let isConnected = false
+let isConnected = false;
 
-export const connectToDB = async ()=>{
-mongoose.set('strictQuery' , true)
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
 
-if(isConnected){
-    console.log('MongoDB is already Connected');
- return
-}
+  if (isConnected) {
+    console.log("MongoDB is already Connected");
+    return;
+  }
 
-try {
-    await mongoose.connect(process.env.MONGODB_URI,{
-        dbName : "share_prompt",
-        useNewUrlParser : true,
-        useUnifiedTopology: true
-    })
-    isConnected  = true
-    console.log('mongoDB connected');
-} catch (error) {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("mongoDB connected");
+  } catch (error) {
     console.log(error);
-}
-}
+  }
+};
+/// Database gets pause automatically after some time. Need to be checked in a while
